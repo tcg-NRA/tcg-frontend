5b6cffcf5865b059dab58b4ea8df25f69c3c3319
Pat <tcg.nexus.regal.alliance@gmail.com>
2025-06-06 19:22:59 -0400

    update over page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import CardPage from './pages/CardPage';
+import CoverPage from './pages/CoverPage';          // ✅ New homepage
+import CardPage from './pages/CardPage';            // Optional: keep at /cards
 import CardDetailPage from './pages/CardDetailPage';
 import DownloadPage from './pages/DownloadPage';
 
@@ -8,12 +9,14 @@ function App() {
   return (
     <Router>
       <nav style={{ padding: '1rem', backgroundColor: '#282c34' }}>
-        <Link to="/" style={{ marginRight: '1rem', color: '#61dafb' }}>Cards</Link>
+        <Link to="/" style={{ marginRight: '1rem', color: '#61dafb' }}>Home</Link>
+        <Link to="/cards" style={{ marginRight: '1rem', color: '#61dafb' }}>Card List</Link>
         <Link to="/download" style={{ color: '#61dafb' }}>Download</Link>
       </nav>
 
       <Routes>
-        <Route path="/" element={<CardPage />} />
+        <Route path="/" element={<CoverPage />} />               {/* 👈 Now default homepage */}
+        <Route path="/cards" element={<CardPage />} />           {/* 👈 Old page now at /cards */}
         <Route path="/cards/:id" element={<CardDetailPage />} />
         <Route path="/download" element={<DownloadPage />} />
       </Routes>
