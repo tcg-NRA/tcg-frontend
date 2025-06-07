523e86f0cfa53ace04c95a18fe20ed17e9cf5075
Pat <tcg.nexus.regal.alliance@gmail.com>
2025-06-07 19:52:41 -0400

    Create cards.js

---
+++ src/data/cards.js
@@ -0,0 +1,14 @@
+// src/data/cards.js
+export const cards = [
+  {
+    _id: "pr_000_1",
+    name: "PR 000 - Alt 1",
+    public_id: "cards_art/pr_000_1"
+  },
+  {
+    _id: "pr_000_2",
+    name: "PR 000 - Alt 2",
+    public_id: "cards_art/pr_000_2"
+  },
+  // …etc, one object per card
+];
